44460621a8f6ba1dd014a4d42c1f324e6a51cea0
gavrilenkoanton <59771981+gavrilenkoanton@users.noreply.github.com>
2020-04-02 13:05:11 +0300

    Updates

---
+++ precache-manifest.660c93274293f915a29fe974e16f054e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee74289b964b3968b5f3373d0c698c7",
+    "url": "/portfolio_gavrilenko/index.html"
+  },
+  {
+    "revision": "24280c31c91a56498ffa",
+    "url": "/portfolio_gavrilenko/static/css/main.287ccd78.chunk.css"
+  },
+  {
+    "revision": "de593dd3ad5a7e2e1171",
+    "url": "/portfolio_gavrilenko/static/js/2.7d7e3c00.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio_gavrilenko/static/js/2.7d7e3c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24280c31c91a56498ffa",
+    "url": "/portfolio_gavrilenko/static/js/main.39d29b42.chunk.js"
+  },
+  {
+    "revision": "565973a9b52a9fc665f2",
+    "url": "/portfolio_gavrilenko/static/js/runtime-main.d1f05778.js"
+  },
+  {
+    "revision": "15a823ef6abd9661a0ae1a40767a2010",
+    "url": "/portfolio_gavrilenko/static/media/architecture_buildings_city_flowers_lamppost_landscape_light_locomotive-1542845.15a823ef.jpg"
+  },
+  {
+    "revision": "f1b60b26278b26afeb0522fa84c23677",
+    "url": "/portfolio_gavrilenko/static/media/css.f1b60b26.svg"
+  },
+  {
+    "revision": "db4608f307e9921f1ca27617bfea46d7",
+    "url": "/portfolio_gavrilenko/static/media/github.db4608f3.svg"
+  },
+  {
+    "revision": "5849bb0d61c2da15b5991dcacafc8e86",
+    "url": "/portfolio_gavrilenko/static/media/html-5.5849bb0d.svg"
+  },
+  {
+    "revision": "d35e4a40fd9e37fe0cd895703aa7e08d",
+    "url": "/portfolio_gavrilenko/static/media/instagram.d35e4a40.svg"
+  },
+  {
+    "revision": "6398b9432ef5cd7bcf8a138eac432086",
+    "url": "/portfolio_gavrilenko/static/media/javascript.6398b943.svg"
+  },
+  {
+    "revision": "a4bdc429f9234fd47424af2f72fb0d7e",
+    "url": "/portfolio_gavrilenko/static/media/linkedin.a4bdc429.svg"
+  },
+  {
+    "revision": "51eaddc9a28ed376fd8e554674be43a5",
+    "url": "/portfolio_gavrilenko/static/media/react.51eaddc9.svg"
+  },
+  {
+    "revision": "80dc6d194ca0e1a876d960ab51becdf2",
+    "url": "/portfolio_gavrilenko/static/media/redux.80dc6d19.svg"
+  },
+  {
+    "revision": "485ad15c26e798892943f71306bf11d5",
+    "url": "/portfolio_gavrilenko/static/media/telegram.485ad15c.svg"
+  },
+  {
+    "revision": "11d7ab9a723dde7ed1a6f81b1b6b8215",
+    "url": "/portfolio_gavrilenko/static/media/typescript.11d7ab9a.svg"
+  }
+]);
